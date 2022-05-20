c6e98b68dbd7e88e31cd24c4b47267db423cc800
Al-Ahmadgaid Asaad <alahmadgaid@gmail.com>
2022-05-20 11:14:03 +0800

    added API for isfeat

--- src/orthography/orthography.jl
+++ src/orthography/orthography.jl
@@ -95,6 +95,34 @@ macro data(expr)
     return esc(Meta.parse(string(expr) * ".data"))
 end
 
+"""
+    isfeat(x::Orthography, y::Type{<:AbstractConsonant})
+
+checks if x is a y feature.
+```julia-repl
+julia> ar_basmala = "بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ";
+julia> arb_token = tokenize(ar_basmala)
+4-element Vector{String}:
+ "بِسْمِ"
+ "ٱللَّهِ"
+ "ٱلرَّحْمَٰنِ"
+ "ٱلرَّحِيمِ"
+julia> arb_parsed2 = parse.(Orthography, arb_token)
+4-element Vector{Orthography}:
+ Orthography(Type[Ba, Kasra, Seen, Sukun, Meem, Kasra])
+ Orthography(Type[AlifHamzatWasl, Lam, Lam, Shadda, Fatha, Ha, Kasra])
+ Orthography(Type[AlifHamzatWasl, Lam, Ra, Shadda, Fatha, HHa, Sukun, Meem, Fatha, AlifKhanjareeya, Noon, Kasra])
+ Orthography(Type[AlifHamzatWasl, Lam, Ra, Shadda, Fatha, HHa, Kasra, Ya, Meem, Kasra])
+julia> isfeat(arb_parsed2[1], AbstractLunar)
+6-element BitVector:
+ 1
+ 0
+ 0
+ 0
+ 1
+ 0
+```
+"""
 function isfeat(x::Orthography, y::Type{<:AbstractConsonant})
     return x.data .<: y
 end
