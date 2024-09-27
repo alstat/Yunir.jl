2ae1da651b5f14e3df8b4bda9b8309f2d319ea37
alstat <alahmadgaid@gmail.com>
2024-09-27 11:17:48 +0800

    update

--- nb/nb3.jl
+++ nb/nb3.jl
@@ -8,14 +8,14 @@ end
 
 const AR_VOWELS = [
     Harakaat(Char(0x064B)[1], true),
-    Harakaat(Char(0x064C)[1], true), 
+    Harakaat(Char(0x064C)[1], true),
     Harakaat(Char(0x064D)[1], true),
     Harakaat(Char(0x064E)[1], false),
-    Harakaat(Char(0x064F)[1], false), 
-    Harakaat(Char(0x0650)[1], false), 
+    Harakaat(Char(0x064F)[1], false),
+    Harakaat(Char(0x0650)[1], false),
 ]
 
-function Base.string(x::Harakaat) 
+function Base.string(x::Harakaat)
     Harakaat(string(x.char), x.is_tanwin)
 end
 
@@ -29,24 +29,15 @@ end
 
 const BW_VOWELS = encode.(string.(AR_VOWELS))
 
-crps, tnzl = load(QuranData());
-crpstbl = table(crps)
-tnzltbl = table(tnzl)
-
-# -----------------
-# Surah Al-Fatihah
-# -----------------
-
-### New approach
 struct Segment
     text::String
     harakat::Array{Harakaat}
 end
 
-struct Syllable
-    lead_nchars::Int64
-    trail_nchars::Int64
-    nvowels::Int64
+struct Syllable{T <: Number}
+    lead_nchars::T
+    trail_nchars::T
+    nvowels::T
 end
 
 struct Rhyme
@@ -55,76 +46,68 @@ struct Rhyme
 end
 
 
-function count_vowels(text, vowels=BW_VOWELS)
+function count_vowels(text::String, vowels::Array{Harakaat}=BW_VOWELS)
     jvowels = join([c.char for c in vowels])
     return count(c -> c in jvowels, text)
 end
 
-function (r::Rhyme)(text::String, verbose::Bool=false)
-    segments = Segment[]
-    segment = Harakaat[]; i = 0; j = 0
+function vowel_indices(s::String, vowels::Array{Harakaat}=BW_VOWELS)
+    jvowels = join([c.char for c in vowels])
+    return findall(c -> c in jvowels, s)
+end
+
+function (r::Rhyme)(text::String)
+    vowel_idcs = vowel_indices(text)
+    harakaat = Harakaat[]
     segment_text = ""
-    
+
     if r.is_quran
         cond = string(text[end]) .∈ BW_VOWELS
         silent_vowel = sum(cond)
+        penalty = sum(cond) < 1 ? 1 : 0 
     end
-    
-    uplimit = r.last_syllable.nvowels > (count_vowels(text) - silent_vowel) ? (count_vowels(text) - 1) : r.last_syllable.nvowels
-    while length(segment) < uplimit
-        try
-            cond = string(text[end-i]) .∈ BW_VOWELS
+
+    uplimit = r.last_syllable.nvowels > (count_vowels(text) - silent_vowel) ? (count_vowels(text) - silent_vowel) : r.last_syllable.nvowels
+    for i in 0:uplimit - silent_vowel - penalty
+        vowel_idx = vowel_idcs[end - i - silent_vowel]
+        cond = string(text[vowel_idx]) .∈ BW_VOWELS
+        if sum(cond) > 0
+            push!(harakaat, BW_VOWELS[cond][1])
+            
+            lead_length = length(text[1:(vowel_idx-1)])
+            trail_length = length(text[(vowel_idx+1):end])
+
+            lead_nchars_lwlimit = lead_length > r.last_syllable.lead_nchars ? r.last_syllable.lead_nchars : lead_length
+            trail_nchars_uplimit = trail_length > r.last_syllable.trail_nchars ? r.last_syllable.trail_nchars : trail_length
+            vowel = text[vowel_idx]
+
+            lead_text = text[(vowel_idx-lead_nchars_lwlimit):(vowel_idx-1)]
+            trail_text = text[(vowel_idx+1):(vowel_idx+trail_nchars_uplimit)]
+
             if i == 0
-                if sum(cond) > 0
-                    i += 1
-                    continue
-                end
-                i += 1
+                segment_text = lead_text * vowel * trail_text
             else
-                if sum(cond) > 0
-                    push!(segment, BW_VOWELS[cond][1])
-                    j += 1
-
-                    # todo: this will go out of bounce for big lead or trail nchars
-                    lead_chars = join([text[end-i-k] for k in 1:r.last_syllable.lead_nchars])
-                    trail_chars = join([text[end-i+k] for k in 1:r.last_syllable.trail_nchars])
-                    new_segment = lead_chars * segment[j].char * trail_chars
-
-                    if j < 2
-                        segment_text = new_segment * segment_text
-                    else 
-                        segment_text = new_segment * "?" * segment_text
-                    end
-                    push!(segments, Segment(segment_text, segment))
-                else
-                    i += 1 
-                    continue
-                end
-                i += 1 
+                segment_text = lead_text * vowel * trail_text * "?" * segment_text
             end
-        catch
-            string(text)
+        else
             continue
         end
     end
-
-    if verbose
-        return segments
-    else
-        return segments[end]
-    end
+    return Segment(segment_text, reverse(harakaat))
 end
 
-bw_texts = encode.(verses(tnzltbl[1]))
+crps, tnzl = load(QuranData());
+crpstbl = table(crps)
+tnzltbl = table(tnzl)
+bw_texts = encode.(verses(tnzltbl))
 
 texts = String[]
 for text in bw_texts
     push!(texts, split(text, " ")[end])
 end
-texts
-text = texts[1]
-count_vowels(text)
-r = Rhyme(true, Syllable(1, 1, 2))
+arabic.(texts)
 
+r = Rhyme(true, Syllable(1, 1, 3))
+segments = r.(texts)
 
-r.(texts)
+[arabic(o.text) for o in out]
--- src/analysis/rhythmic/utils.jl
+++ src/analysis/rhythmic/utils.jl
@@ -1,72 +1,108 @@
-# include("../../constants.jl")
+"""
+	Harakaat(s::String)
 
-using Makie
-using CairoMakie
-using QuranTree
-using Yunir
+tokenizes the string input s by space, and also tokenizes the punctuations.
+```julia-repl
+julia> ar_basmala = "بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ"
+julia> tokenize(ar_basmala)
+4-element Vector{String}:
+ "بِسْمِ"
+ "ٱللَّهِ"
+ "ٱلرَّحْمَٰنِ"
+ "ٱلرَّحِيمِ"
+```
+"""
+struct Harakaat
+	char::Union{String, Char}
+	is_tanwin::Bool
+end
 
-struct Syllables
-    char_idx1::Int64
-    char_idx2::Int64
+const AR_VOWELS = [
+	Harakaat(Char(0x064B)[1], true),
+	Harakaat(Char(0x064C)[1], true),
+	Harakaat(Char(0x064D)[1], true),
+	Harakaat(Char(0x064E)[1], false),
+	Harakaat(Char(0x064F)[1], false),
+	Harakaat(Char(0x0650)[1], false),
+]
+
+function Base.string(x::Harakaat)
+	Harakaat(string(x.char), x.is_tanwin)
+end
+
+Yunir.encode(x::Harakaat) = Harakaat(encode(string(x.char)), x.is_tanwin)
+
+Base.occursin(x::String, y::Harakaat) = occursin(x, y.char)
+
+function Base.broadcasted(::typeof(in), s::AbstractString, mss::Vector{Harakaat})
+	return [occursin(s, ms) for ms ∈ mss]
+end
+
+const BW_VOWELS = encode.(string.(AR_VOWELS))
+
+struct Segment
+	text::String
+	harakat::Array{Harakaat}
+end
+
+struct Syllable{T <: Number}
+	lead_nchars::T
+	trail_nchars::T
+	nvowels::T
 end
 
 struct Rhyme
-    texts::Vector{String}
-    syllables_indices::Syllables
-end
-
-function last_syllables(r::Rhyme)
-    syllables = Vector{String}()
-    for texts in r.texts
-        push!(syllables, texts[(end-r.syllables_indices.char_idx2):(end-r.syllables_indices.char_idx1)])
-    end
-    return syllables
-end
-
-function transition(r::Rhyme)
-    syllables = last_syllables(r)
-    y = unique(syllables)
-    y_dict = Dict{String,Int64}()
-    for i in eachindex(y)
-        if i == 1
-            y_dict[y[i]] = i
-        end
-        
-        if y[i] .∈ Ref(Set(keys(y_dict)))
-            continue
-        else
-            y_dict[y[i]] = i
-        end
-    end
-    y_vec = Vector{Int64}()
-    for i in syllables
-        push!(y_vec, y_dict[i])
-    end
-    return y_vec, y_dict 
+	is_quran::Bool
+	last_syllable::Syllable
 end
 
 
-crps, tnzl = load(QuranData());
-crpstbl = table(crps)
-tnzltbl = table(tnzl)
+function count_vowels(text::String, vowels::Array{Harakaat} = BW_VOWELS)
+	jvowels = join([c.char for c in vowels])
+	return count(c -> c in jvowels, text)
+end
 
-# -----------------
-# Surah Al-Fatihah
-# -----------------
-bw_texts = encode.(verses(tnzltbl[1]))
+function vowel_indices(s::String, vowels::Array{Harakaat} = BW_VOWELS)
+	jvowels = join([c.char for c in vowels])
+	return findall(c -> c in jvowels, s)
+end
 
-r = Rhyme(replace.(bw_texts, "o" => ""), Syllables(1, 4))
-y, ydict = transition(r)
+function (r::Rhyme)(text::String)
+	vowel_idcs = vowel_indices(text)
+	harakaat = Harakaat[]
+	segment_text = ""
 
-# plotting
+	if r.is_quran
+		cond = string(text[end]) .∈ BW_VOWELS
+		silent_vowel = sum(cond)
+		penalty = sum(cond) < 1 ? 1 : 0
+	end
 
-f = Figure(resolution=(500, 500));
-a1 = Axis(f[1,1], 
-    xlabel="Ayah Number",
-    ylabel="Last Pronounced Syllable\n\n\n",
-    title="Surah Al-Fatihah Rhythmic Patterns\n\n",
-    yticks=(unique(y), unique(last_syllables(r))), 
-    xticks = collect(eachindex(last_syllables(r)))
-)
-lines!(a1, collect(eachindex(last_syllables(r))), y)
-f
+	uplimit = r.last_syllable.nvowels > (count_vowels(text) - silent_vowel) ? (count_vowels(text) - silent_vowel) : r.last_syllable.nvowels
+	for i in 0:uplimit-silent_vowel-penalty
+		vowel_idx = vowel_idcs[end-i-silent_vowel]
+		cond = string(text[vowel_idx]) .∈ BW_VOWELS
+		if sum(cond) > 0
+			push!(harakaat, BW_VOWELS[cond][1])
+
+			lead_length = length(text[1:(vowel_idx-1)])
+			trail_length = length(text[(vowel_idx+1):end])
+
+			lead_nchars_lwlimit = lead_length > r.last_syllable.lead_nchars ? r.last_syllable.lead_nchars : lead_length
+			trail_nchars_uplimit = trail_length > r.last_syllable.trail_nchars ? r.last_syllable.trail_nchars : trail_length
+			vowel = text[vowel_idx]
+
+			lead_text = text[(vowel_idx-lead_nchars_lwlimit):(vowel_idx-1)]
+			trail_text = text[(vowel_idx+1):(vowel_idx+trail_nchars_uplimit)]
+
+			if i == 0
+				segment_text = lead_text * vowel * trail_text
+			else
+				segment_text = lead_text * vowel * trail_text * "?" * segment_text
+			end
+		else
+			continue
+		end
+	end
+	return Segment(segment_text, reverse(harakaat))
+end
